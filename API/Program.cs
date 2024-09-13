76e8d3a1843f3fd3a0519e1fcb0f66d3c5a6fb58
gr1gor1ychuk <grigorijchuk.den@gmail.com>
2024-09-13 15:29:13 +0300

    End of section 5

--- API/Program.cs
+++ API/Program.cs
@@ -5,13 +5,25 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
+builder.Services.AddCors(options => 
+{
+    options.AddPolicy("client", policyBuilder => 
+    {
+        policyBuilder.AllowAnyHeader();
+        policyBuilder.AllowAnyMethod();
+        policyBuilder.WithOrigins("https://localhost:4200");
+        policyBuilder.AllowCredentials();        
+    });
+});
 
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
-    .WithOrigins("http://localhost:4200","https://localhost:4200)"));
+// app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+//     .WithOrigins("http://localhost:4200","https://localhost:4200)"));
+
+app.UseCors("client");
 
 app.UseAuthentication();
 app.UseAuthorization();
